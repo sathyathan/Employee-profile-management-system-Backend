cec2d7e76a4e77465104cd1a9444f6d9d145788a
sathaythan <sathyathangavel7@gmail.com>
2024-06-26 13:13:17 +0530

    updated

--- Models/employeeSchema.js
+++ Models/employeeSchema.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const employeeSchema=new mongoose.Schema({
+     firstname:String,
+     lastname:String,
+     designation:String,
+     role:String,
+     email:String,
+     password:String,
 
-     Emp_firstname:String,
-     Emp_lastname:String,
-     Email:String,
-     Emp_batch:String,
-     Emp_designation:String,
-     Emp_role:String,
-     Password:String,
+     
 })
 const Employee=mongoose.model("Employee",employeeSchema);
 
